76082778a9ca2ab3fd1e7519b44d57da2b1d06fb
yashk2000 <41234408+yashk2000@users.noreply.github.com>
2018-10-28 10:51:14 +0530

    Update gsearch.py

--- gsearch.py
+++ gsearch.py
@@ -3,13 +3,14 @@ import sys
 
 search_item=input("enter search parameter and print Ctrl-C to stop :")#getting search parameter
 # searching on "http://www.google.co.in"
+engine="http://www.google.co.in"
 url="http://www.google.co.in/search?q="+ search_item#obtaining url of search parameter
 response=requests.get(url)#getting a web page
 soup=BeautifulSoup(response.text,"lxml")#processing the html page into text using lxml
 for item in soup.select(".r a"):#printing results on first page
 	print(item.text)
 for next_page in soup.select(".fl"):#going to next page
-	res=requests.get(base + next_page.get('href'))#obtaining the next web page
+	res=requests.get(engine+next_page.get('href'))#obtaining the next web page
 	soup= BeautifulSoup(res.text,"lxml")#processing the web page
 	for item in soup.select(".r a"):#printing the results on that page
 		print(item.text)
